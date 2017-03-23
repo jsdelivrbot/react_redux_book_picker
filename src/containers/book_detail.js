8ea0303444e5c36f5f4a48c0fdb26117dc3ca89a
Niall Maher <Niall.Maher@amdocs.com>
2017-03-23 18:57:01 +0000

    added a small bit of styling

--- src/containers/book_detail.js
+++ src/containers/book_detail.js
@@ -5,12 +5,12 @@ class BookDetail extends Component {
   render(){
     if (!this.props.book){
         return (
-          <div>
+          <div className="details_container">
             Select a book to get started!
           </div>);
     }
     return (
-      <div>
+      <div className="details_container">
         <h3>Details for:</h3>
         <div>Title: {this.props.book.title}</div>
         <div>Pages: {this.props.book.pages}</div>
